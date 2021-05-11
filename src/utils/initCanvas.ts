c86adfbdbda9b6667e62eef21ca40d7273f6a1f9
btea <2356281422@qq.com>
2021-05-11 14:43:59 +0800

    feat: init canvas

---
+++ src/utils/initCanvas.ts
@@ -0,0 +1,34 @@
+interface canvasInfo {
+	ctx?: CanvasRenderingContext2D;
+	width?: number;
+	height?: number;
+	ratio?: number;
+}
+type obj = canvasInfo
+
+export function initCanvas(
+	canvas: HTMLCanvasElement,
+	width: number,
+	height: number
+): canvasInfo {
+	const obj: obj = {};
+	const w = width;
+	const h = height;
+	obj.ctx = canvas.getContext('2d');
+	obj.width = w;
+	obj.height = h;
+	const context = obj.ctx as CanvasRenderingContext2D;
+	// 根据设备像素比优化canvas绘图
+	const devicePixelRatio = window.devicePixelRatio || 1;
+	obj.ratio = devicePixelRatio;
+	Object.assign(canvas, {
+		style: {
+			width: `${w}px`,
+			height: `${h}px`
+		},
+		width: w * devicePixelRatio,
+		height: h * devicePixelRatio
+	});
+	context.scale(devicePixelRatio, devicePixelRatio);
+	return obj;
+}
