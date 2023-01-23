54d0440ff8adc441c49daef5beb7ce7ec28e3a79
btea <2356281422@qq.com>
2023-01-23 17:46:42 +0800

    chore: update dep

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
+import dns from 'node:dns'
+
+dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: true
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
